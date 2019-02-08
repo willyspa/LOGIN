fb567c21e5f39272d40c43a574f9495de6dfa431
willyspa <armed95@hotmail.fr>
2019-02-08 07:07:07 +0100

    readme

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
   def create
+    "@"*60
     puts params
+      "@"*60
+
 
     if params[:password] == params[:password_confirmation]
-      u = User.new(city_id:1, first_name:params[:first_name], last_name:params[:last_name], email:params[:email],password:params[:password])
+      my_city = City.find_by(name:params[:name])
+
+      u = User.new(city_id:my_city.id, first_name:params[:first_name], last_name:params[:last_name], email:params[:email],password:params[:password])
       puts "*"*60
       puts u.password_digest
       puts "*"*60
@@ -15,9 +20,11 @@ class UsersController < ApplicationController
 
   end
   def new
+    @city = City.all
     @user = User.new
   end
   def show
+    @city = City.all
     id_params = params[:id].to_i
     @array_user = User.all
     @user = @array_user[id_params-1]
